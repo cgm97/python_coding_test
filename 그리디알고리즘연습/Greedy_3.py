44f19bb9f6520aff21a16595f36cced4f375d1f5
kyumin <cgm97@naver.com>
2021-01-13 02:45:59 +0900

    Greedy

--- 그리디알고리즘연습/Greedy_3.py
+++ 그리디알고리즘연습/Greedy_3.py
@@ -15,12 +15,12 @@ for n in range(n) :
 print(result)
 
 # 2중 반복문 사용
-# for n in range(n) :
-#     data = list(map(int,input().split()))
-#     min_value = 10001
-#     for m in data :
-#         # n행의 가장 작은 수
-#         min_value = min(min_value,m)
-#     # n행들의 가장 작은 수 중의 제일 큰값
-#     result = max(result,min_value)
-# print(result)
+for n in range(n) :
+    data = list(map(int,input().split()))
+    min_value = 10001
+    for m in data :
+        # n행의 가장 작은 수
+        min_value = min(min_value,m)
+    # n행들의 가장 작은 수 중의 제일 큰값
+    result = max(result,min_value)
+print(result)
